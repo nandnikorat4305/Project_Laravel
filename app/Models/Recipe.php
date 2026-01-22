9729748a1eb36acc8845cb0e072d9544743379f6
nandnikorat4305 <nandnikorat4305@gmail.com>
2026-01-22 21:11:30 +0530

    Add files via upload

---
+++ app/Models/Recipe.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'category',
+        'description',
+        'ingredients',
+        'steps',
+        'image',
+    ];
+}
